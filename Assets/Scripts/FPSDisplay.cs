683872af197e446313d3f668e14d648814251ec5
mofr <mofr@zond.org>
2015-09-08 21:33:17 +0600

    Add score ui mockup; Simplify FPSDisplay;

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class FPSDisplay : MonoBehaviour {
-
-	public int fontSize = 12;
-	public Color color = Color.white;
+	
+	public GUIStyle style;
 
 	float deltaTime = 0;
 
@@ -13,12 +12,7 @@ public class FPSDisplay : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUIStyle style = new GUIStyle();
-		
 		Rect rect = new Rect(0, 0, Screen.width, Screen.height);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = fontSize;
-		style.normal.textColor = color;
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
 		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
